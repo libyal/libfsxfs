1680cb62dba1c0676948e12d34ea43ef7f48155f
Joachim Metz <joachim.metz@gmail.com>
2020-12-24 13:54:27 +0100

    Applied updates and code clean up

--- libfsxfs/libfsxfs_btree_block.c
+++ libfsxfs/libfsxfs_btree_block.c
@@ -389,27 +389,11 @@ int libfsxfs_btree_block_read_file_io_handle(
 		 file_offset );
 	}
 #endif
-	if( libbfio_handle_seek_offset(
-	     file_io_handle,
-	     file_offset,
-	     SEEK_SET,
-	     error ) == -1 )
-	{
-		libcerror_error_set(
-		 error,
-		 LIBCERROR_ERROR_DOMAIN_IO,
-		 LIBCERROR_IO_ERROR_SEEK_FAILED,
-		 "%s: unable to seek B+ tree block offset: %" PRIi64 " (0x%08" PRIx64 ").",
-		 function,
-		 file_offset,
-		 file_offset );
-
-		return( -1 );
-	}
-	read_count = libbfio_handle_read_buffer(
+	read_count = libbfio_handle_read_buffer_at_offset(
 	              file_io_handle,
 	              btree_block->data,
 	              btree_block->data_size,
+	              file_offset,
 	              error );
 
 	if( read_count != (ssize_t) btree_block->data_size )
--- libfsxfs/libfsxfs_extern.h
+++ libfsxfs/libfsxfs_extern.h
@@ -30,7 +30,7 @@
 
 #include <libfsxfs/extern.h>
 
-#if defined( __CYGWIN__ )
+#if defined( __CYGWIN__ ) || defined( __MINGW32__ )
 #define LIBFSXFS_EXTERN_VARIABLE	extern
 #else
 #define LIBFSXFS_EXTERN_VARIABLE	LIBFSXFS_EXTERN
