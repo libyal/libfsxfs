3d0bc612d36ed448d793a65ed3f4c02dc54b9b7a
Joachim Metz <joachim.metz@gmail.com>
2022-10-10 19:42:48 +0200

    Applied updates and worked on tests

--- ossfuzz/file_entry_fuzzer.cc
+++ ossfuzz/file_entry_fuzzer.cc
@@ -47,11 +47,24 @@ int LLVMFuzzerTestOneInput(
      const uint8_t *data,
      size_t size )
 {
+	uint8_t string_value[ 64 ];
+
 	libbfio_handle_t *file_io_handle      = NULL;
 	libfsxfs_file_entry_t *root_directory = NULL;
 	libfsxfs_file_entry_t *sub_file_entry = NULL;
 	libfsxfs_volume_t *volume             = NULL;
+	size64_t file_size                    = 0;
+	size_t string_size                    = 0;
+	off64_t file_offset                   = 0;
+	uint64_t value_64bit                  = 0;
+	int64_t posix_time                    = 0;
+	uint32_t major_device_number          = 0;
+	uint32_t minor_device_number          = 0;
+	uint32_t value_32bit                  = 0;
+	uint16_t value_16bit                  = 0;
+	int number_of_extents                 = 0;
 	int number_of_sub_file_entries        = 0;
+	int result                            = 0;
 
 	if( libbfio_memory_range_initialize(
 	     &file_io_handle,
@@ -95,13 +108,106 @@ int LLVMFuzzerTestOneInput(
 		}
 		if( number_of_sub_file_entries > 0 )
 		{
-			if( libfsxfs_file_entry_get_sub_file_entry_by_index(
-			     root_directory,
-			     0,
-			     &sub_file_entry,
-			     NULL ) != 1 )
+			result = libfsxfs_file_entry_get_sub_file_entry_by_index(
+			          root_directory,
+			          0,
+			          &sub_file_entry,
+			          NULL );
+
+			if( result != -1 )
 			{
-				goto on_error_libfsxfs_root_directory;
+				libfsxfs_file_entry_get_inode_number(
+				 sub_file_entry,
+				 &value_64bit,
+				 NULL );
+
+				libfsxfs_file_entry_get_creation_time(
+				 sub_file_entry,
+				 &posix_time,
+				 NULL );
+
+				libfsxfs_file_entry_get_modification_time(
+				 sub_file_entry,
+				 &posix_time,
+				 NULL );
+
+				libfsxfs_file_entry_get_access_time(
+				 sub_file_entry,
+				 &posix_time,
+				 NULL );
+
+				libfsxfs_file_entry_get_inode_change_time(
+				 sub_file_entry,
+				 &posix_time,
+				 NULL );
+
+				libfsxfs_file_entry_get_file_mode(
+				 sub_file_entry,
+				 &value_16bit,
+				 NULL );
+
+				libfsxfs_file_entry_get_number_of_links(
+				 sub_file_entry,
+				 &value_32bit,
+				 NULL );
+
+				libfsxfs_file_entry_get_owner_identifier(
+				 sub_file_entry,
+				 &value_32bit,
+				 NULL );
+
+				libfsxfs_file_entry_get_group_identifier(
+				 sub_file_entry,
+				 &value_32bit,
+				 NULL );
+
+				libfsxfs_file_entry_get_device_identifier(
+				 sub_file_entry,
+				 &value_32bit,
+				 NULL );
+
+				libfsxfs_file_entry_get_device_number(
+				 sub_file_entry,
+				 &major_device_number,
+				 &minor_device_number,
+				 NULL );
+
+				libfsxfs_file_entry_get_utf8_name_size(
+				 sub_file_entry,
+				 &string_size,
+				 NULL );
+
+				libfsxfs_file_entry_get_utf8_name(
+				 sub_file_entry,
+				 string_value,
+				 64,
+				 NULL );
+
+				libfsxfs_file_entry_get_utf8_symbolic_link_target_size(
+				 sub_file_entry,
+				 &string_size,
+				 NULL );
+
+				libfsxfs_file_entry_get_utf8_symbolic_link_target(
+				 sub_file_entry,
+				 string_value,
+				 64,
+				 NULL );
+
+				libfsxfs_file_entry_get_offset(
+				 sub_file_entry,
+				 &file_offset,
+				 NULL );
+
+				libfsxfs_file_entry_get_size(
+				 sub_file_entry,
+				 &file_size,
+				 NULL );
+
+				libfsxfs_file_entry_get_number_of_extents(
+				 sub_file_entry,
+				 &number_of_extents,
+				 NULL );
 			}
 			libfsxfs_file_entry_free(
 			 &sub_file_entry,
