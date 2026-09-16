2c53b1c9fac4006a2564008cd11f330b6a7f8a03
Joachim Metz <joachim.metz@gmail.com>
2026-09-16 08:05:04 +0200

    Worked on tests and changes for CI tests

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,21 +1,22 @@
 #!/bin/sh
 # Script to run tests
 #
-# Version: 20260609
+# Version: 20260714
 
-if test -f ${PWD}/libfsxfs/.libs/libfsxfs.1.dylib && test -f ./pyfsxfs/.libs/pyfsxfs.so
+if [ -f "${PWD}/libfsxfs/.libs/libfsxfs.1.dylib" ] && [ -f ./pyfsxfs/.libs/pyfsxfs.so ]
 then
-	install_name_tool -change /usr/local/lib/libfsxfs.1.dylib ${PWD}/libfsxfs/.libs/libfsxfs.1.dylib ./pyfsxfs/.libs/pyfsxfs.so
+    install_name_tool -change /usr/local/lib/libfsxfs.1.dylib "${PWD}/libfsxfs/.libs/libfsxfs.1.dylib" ./pyfsxfs/.libs/pyfsxfs.so
 fi
 
 make check-build > /dev/null
 
+# shellcheck disable=SC2068
 make check $@
 RESULT=$?
 
-if test ${RESULT} -ne 0
+if [ ${RESULT} -ne 0 ]
 then
-	find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
+    find . -name \*.log -path \*.dir/\*/\*.log -print -exec cat {} \;
 fi
 exit ${RESULT}
 
